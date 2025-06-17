ce8ff8ab43cf60434aff9d0196e483a466002afb
collins otieno <collins.ouma2@student.moringaschool.com>
2025-06-17 17:15:18 +0300

    dom nodifying 1st commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'COLLINS OTIENO is the champion'; 
+
+
+document.body.appendChild(newHeader);
